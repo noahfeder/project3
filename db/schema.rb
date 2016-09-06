b229f359e6bd4205f4183daab5c684f643d4b0dc
Ian Zweig <ianbzweig@gmail.com>
2016-09-06 16:46:47 -0400

    sessions/identity model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160906175106) do
+ActiveRecord::Schema.define(version: 20160906201549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "identities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "user_preferences", force: :cascade do |t|
     t.string   "date_of_birth"
