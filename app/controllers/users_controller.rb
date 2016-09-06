4d34801d12f7d8f6f9f465ffd985292e7270a4a1
Noah Feder <noah.feder@gmail.com>
2016-09-06 15:39:55 -0400

    add helper methods for twitter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :get_trends, only: [:index]
   def index
   end
 
@@ -16,5 +16,14 @@ class UsersController < ApplicationController
     redirect_to "/new"
   end
 
-
+  private
+    # get local and global trends for user
+    # TODO Caching
+    def get_trends
+      @ip = "8.8.8.8" # TODO change to dynamic, based on request.remote_ip
+      @ll = Geocoder.coordinates(@ip) # TODO preference storing coordinates on signup
+      @tl = twitter.trends_closest(lat: @ll[0], long: @ll[1])[0].id # TODO preference storing id on signup
+      @local_trends = twitter.trends(id = @tl) #local trends, TODO add redis
+      @global_trends = twitter.trends(id = 1) #global trends, TODO add redis
+    end
 end
