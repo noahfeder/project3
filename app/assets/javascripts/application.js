55651a1b3379f4f94026a295f0b196bb6d9b73bb
Noah Feder <noah.feder@gmail.com>
2016-09-08 00:06:25 -0400

    add comments and clean up stray code bits

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,38 +15,40 @@
 //= require turbolinks
 //= require_tree .
 
+// TODO move this into a users.js file
+// (if we add other features loaded out of this order)
 $(document).ready(function() {
 
-  console.log("ready!");
-
-  $('.tweets').on('click',function() {
+  // rotate to show other side of tweetset "card"
+  $('.tweets').click(function() {
     $(this).toggleClass('rotate');
-  })
-
+  });
+  // create a new todo
   $(".submit_item").click(function() {
     submitForm();
   });
-
+  // prevent form from actually submitting
   $("#new_todo").submit(function(e){
     e.preventDefault();
   });
-
+  // delete a todo
   $(".delete_todo").click(function() {
     deleteTodo(this);
-  })
-
-  $("input[type='checkbox']").on('change',function() {
+  });
+  // update a todo.completed
+  $("input[type='checkbox']").change(function() {
     updateTodo(this);
-  })
+  });
 
   var submitForm = function(){
+    // data sent via AJAX
     var dataToSend = {
       "user_id" : $("#todo_user_id").val(),
       "item" : $('#todo_item').val(),
       "completed" : 0
     };
 
-      $.ajax({
+    $.ajax({
       "method" : "POST",
       "url" : "/todos",
       "data" : dataToSend,
@@ -56,6 +58,7 @@ $(document).ready(function() {
     });
   };// end of submitForm
 
+  // build new li with TODO information
   var appendItem = function(data) {
     var $items = $(".items");
     var $li = $("<li>");
@@ -66,11 +69,13 @@ $(document).ready(function() {
     })
     var $button = $('<button class="delete_todo">X</button>');
     $span.text(data.item);
+    // sets id as item_1, item_10, item_100, for data retrieval later
     $li.attr('id','item_' + data.id)
     $li.append($checkbox).append($span).append($button).addClass('todo');
     $items.append($li);
   };//end of appendItem
 
+  // remove a todo
   var deleteTodo = function(thing) {
     var parent = $(thing).parent()[0];
     var id = parent.id.split('_')[1];
@@ -83,14 +88,17 @@ $(document).ready(function() {
     });
   }; // end of deleteTodo
 
+  // update current status of todo via checkbox's checked
   var updateTodo = function(thing) {
     var completed = thing.checked;
     var parent = thing.parentElement;
     var id = parent.id.split('_')[1];
+
     var dataToSend = {
       completed: completed,
       id: id
     };
+
     $.ajax({
       "method" : "PATCH",
       "url" : "/todos/" + id,
@@ -98,6 +106,7 @@ $(document).ready(function() {
       "data" : dataToSend
     }).done(function(data) {
       $(parent).children('span').toggleClass('completed');
-    })
-  };
-});
+    });
+  }; // end of updateTodo
+
+}); // end of $(document).ready
